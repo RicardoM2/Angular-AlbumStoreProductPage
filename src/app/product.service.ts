9f283099242d4af386129e56c21fa0b19ab7a235
Ricardo <ricardomiranda0320@gmail.com>
2021-07-26 22:00:38 -0400

    Add fixed in services

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http} from "@angular/http";
+import {Http, Response} from "@angular/http";
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 
@@ -7,11 +7,11 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  private _album = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
    
   constructor( private _http: Http) { }
   
   getAlbum(id: number){
-    return this._http.get(this._album).map((response:any) =>response.json())
+    return this._http.get(this._albumUrl).map((response) =>response.json())
   }
 }
